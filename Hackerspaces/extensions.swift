924fcfe57aaed72aa6ed919b67f623998297f961
zephyz <zephyz.zhaar@gmail.com>
2015-08-06 19:36:02 +0200

    added podfile

---
+++ Hackerspaces/extensions.swift
@@ -0,0 +1,9 @@
+//
+//  extensions.swift
+//  Hackerspaces
+//
+//  Created by zephyz on 06/08/15.
+//  Copyright (c) 2015 Fixme. All rights reserved.
+//
+
+import Foundation
---
+++ Podfile
@@ -0,0 +1,7 @@
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'SwiftHTTP', '~> 0.9.4'
+pod 'JSONJoy-Swift', '~> 0.9.1'
