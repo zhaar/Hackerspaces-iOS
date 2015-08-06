1bb7506f5b8e5663e24c0f1c5dc9adc61046fc7f
zephyz <zephyz.zhaar@gmail.com>
2015-08-06 19:41:45 +0200

    added map kit view

--- Hackerspaces/MapViewController.swift
+++ Hackerspaces/MapViewController.swift
@@ -13,6 +13,18 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var map: MKMapView!
     
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(initialLocation)
+    }
 
 }
 
