b25d7a9681ee6f2fc97b83df60f9f3babf13fc4b
zephyz <zephyz.zhaar@gmail.com>
2015-08-12 04:47:01 +0200

    added functionality to existing primitive types

--- Hackerspaces/extensions.swift
+++ Hackerspaces/extensions.swift
@@ -15,4 +15,62 @@ extension Array {
             fn(e)
         }
     }
+    
+    func foldl<S>(initial: S,fn: (acc:S, elem: T) -> S) -> S {
+        var result = initial
+        for e in self {
+            result = fn(acc: result, elem: e)
+        }
+        return result
+    }
+    
+    func cons(e: Element) -> [Element] {
+        var cpy = self
+        cpy.append(e)
+        return cpy
+    }
+    
+    func groupBy<S>(fn: T -> S) -> [S: [T]] {
+        var dic = [S: [T]]()
+        for e in self {
+            let key = fn(e)
+            dic[key] = dic[key]?.cons(e) ?? [e]
+        }
+        return dic
+    }
+    
+    func immutableSort(isOrderedBefore: (T, T) -> Bool) -> [Element] {
+        var cpy = self
+        cpy.sort(isOrderedBefore)
+        return cpy
+    }
+    
+}
+
+extension Dictionary {
+    
+    func getWithDefault(key: Key, fallback: Value) -> Value {
+        return self[key] ?? fallback
+    }
+    
+    func immutableInsert(key: Key, value val: Value) -> [Key : Value] {
+        var cpy = self
+        cpy[key] = val
+        return cpy
+    }
+}
+
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[advance(self.startIndex, i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        return substringWithRange(Range(start: advance(startIndex, r.startIndex), end: advance(startIndex, r.endIndex)))
+    }
 }
