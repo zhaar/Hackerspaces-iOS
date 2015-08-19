688821683e10d8d5ecbcc02d6c591562c3ddb7a9
zephyz <zephyz.zhaar@gmail.com>
2015-08-20 01:34:12 +0200

    added app icon

--- Hackerspaces/ObjectTableViewController.swift
+++ Hackerspaces/ObjectTableViewController.swift
@@ -6,4 +6,39 @@
 //  Copyright (c) 2015 Fixme. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ObjectTableViewController: UITableViewController {
+    let collation = UILocalizedIndexedCollation.currentCollation() as UILocalizedIndexedCollation
+    var sections: [[Object]] = []
+    var objects: [Object] {
+        didSet {
+            let selector: Selector = "localizedTitle"
+            
+            
+            sections = [[Object]](count: collation.sectionTitles.count, repeatedValue: [])
+            
+            let sortedObjects = collation.sortedArrayFromArray(objects, collationStringSelector: selector) as [Object]
+            for object in sortedObjects {
+                let sectionNumber = collation.sectionForObject(object, collationStringSelector: selector)
+                sections[sectionNumber].append(object)
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    override func tableView(tableView: UITableView!, titleForHeaderInSection section: Int) -> String! {
+        return collation.sectionTitles![section] as String
+    }
+    
+    override func sectionIndexTitlesForTableView(tableView: UITableView!) -> [AnyObject]! {
+        return collation.sectionIndexTitles
+    }
+    
+    override func tableView(tableView: UITableView!, sectionForSectionIndexTitle title: String!, atIndex index: Int) -> Int {
+        return collation.sectionForSectionIndexTitleAtIndex(index)
+    }
+}
