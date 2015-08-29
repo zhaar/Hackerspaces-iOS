ce464e171a51c001a0982e8bcff86c5696e014ab
zephyz <zephyz.zhaar@gmail.com>
2015-08-29 23:49:50 +0200

    replaced segue by tab select

--- Hackerspaces/VCMapView.swift
+++ Hackerspaces/VCMapView.swift
@@ -30,6 +30,13 @@ extension MapViewController: MKMapViewDelegate {
     }
     
     func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
-        performSegueWithIdentifier(UIConstants.ShowHSSegue, sender: view)
+        if let annotation = view.annotation as? SpaceLocation {
+            SpaceAPI.loadAPI().onSuccess { dict in
+                Model.sharedInstance.favoriteHackerspaceURL = dict[annotation.name]
+                self.tabBarController?.selectedIndex = 0
+            }
+        }
+
+//        performSegueWithIdentifier(UIConstants.ShowHSSegue, sender: view)
     }
 }
