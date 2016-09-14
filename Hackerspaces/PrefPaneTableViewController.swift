75e7f16fab039521d146150a29f04e8bb8b7f389
zephyz <zephyz.zhaar@gmail.com>
2016-09-14 20:23:45 +0200

    implement toggle in settings view

---
+++ Hackerspaces/PrefPaneTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewController.swift
+//  Hackerspaces
+//
+//  Created by zephyz on 14/09/16.
+//  Copyright © 2016 Fixme. All rights reserved.
+//
+
+import UIKit
+
+class PrefPaneTableViewController: UITableViewController {
+    
+    @IBAction func toggleDebugMode(sender: UISwitch) {
+        print("toggled")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
