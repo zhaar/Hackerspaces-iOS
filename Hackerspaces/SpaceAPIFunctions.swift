22a9c9d4df0f2b76f10ecb3837afcf57aeb2d9a0
zephyz <zephyz.zhaar@gmail.com>
2017-08-13 17:24:53 +0200

    refactor network and cache functions into compartimented extensions

--- Hackerspaces/SpaceAPIFunctions.swift
+++ Hackerspaces/SpaceAPIFunctions.swift
@@ -15,16 +15,42 @@ import MapKit
 import Haneke
 import JSONWrapper
 
-struct SpaceAPI {
+
+private func parseAPI(data: Data) -> [String: String]? {
+    let parsed = JSONObject.parse(fromData: data)?.asObject
+    let api = parsed.map { $0.flatMap { t in t.value.asString.map { (t.key, $0) } } }
+    return api.map(tuplesAsDict)
+}
+
+/// namespace for all space api methods
+enum SpaceAPI { }
+
+//MARK: - helper functions for automatically loading from cache or network
+extension SpaceAPI {
+    static func loadHackerspaceList(fromCache: Bool) -> Future<[String: String], SpaceAPIError> {
+        return fromCache ? loadAPIFromCache() : (loadAPIFromWeb().recoverWith { _ in print("loading from web failed, loading from cache"); return loadAPIFromCache() })
+    }
+
+    static func loadHackerspaceData(url: String, fromCache: Bool = true) -> Future<[String : JSONValue], SpaceAPIError> {
+        return (fromCache ? SpaceAPI.loadHackerspaceDataFromCache : SpaceAPI.loadHackerspaceDataFromWeb)(url)
+    }
+
+    static func parseHackerspace(json: [String : JSONValue], url: String, name: String) -> Result<ParsedHackerspaceData, SpaceAPIError>{
+        return parseHackerspaceDataModel(json: json, name: name, url: url) |=> .parseError(json.description)
+    }
+
+    static func getParsedHackerspace(url: String, name: String, fromCache cache: Bool = true) -> Future<ParsedHackerspaceData, SpaceAPIError> {
+        return loadHackerspaceData(url: url, fromCache: cache).flatMap { parseHackerspace(json: $0, url: url, name: name) }
+    }
+}
+
+//MARK: - Network requests
+extension SpaceAPI {
 
     enum HTTPError: Error {
         case notOK(HTTPStatusCode)
         case requestError(Error)
         case unknownError
-    }
-
-    enum CacheError: Error {
-        case notFound(key: String, error: Error?)
     }
 
     static private func httpRequest(url: String) -> Future<Data, HTTPError> {
@@ -52,6 +78,30 @@ struct SpaceAPI {
         return p.future
     }
 
+    static fileprivate func loadHackerspaceDataFromWeb(url: String) -> Future<[String : JSONValue], SpaceAPIError> {
+        return httpRequest(url: url)
+            .mapError { SpaceAPIError.httpRequestError(error: $0) }
+            .flatMap({ (data: Data) -> Result<[String : JSONValue], SpaceAPIError> in
+                return JSONObject.parse(fromData: data)?.asObject |=> SpaceAPIError.dataCastError(data: data)
+            })
+    }
+
+    static fileprivate func loadAPIFromWeb() -> Future<[String : String], SpaceAPIError> {
+        return httpRequest(url: SpaceAPIConstants.FIXMEAPI.rawValue)
+            .mapError { SpaceAPIError.httpRequestError(error: $0) }
+            .flatMap { (data: Data) -> Result<[String: String], SpaceAPIError> in
+                return parseAPI(data: data) |=> SpaceAPIError.dataCastError(data: data)
+        }
+    }
+}
+
+//MARK: - Loading from cache
+extension SpaceAPI {
+
+    enum CacheError: Error {
+        case notFound(key: String, error: Error?)
+    }
+
     static private func loadFromCache(key: String) -> Future<Data, CacheError> {
         let p = Promise<Data, CacheError>()
         DispatchQueue.global().async {
@@ -64,58 +114,19 @@ struct SpaceAPI {
         return p.future
     }
 
-    private static func parseAPI(data: Data) -> [String: String]? {
-        let parsed = JSONObject.parse(fromData: data)?.asObject
-        let api = parsed.map { $0.flatMap { t in t.value.asString.map { (t.key, $0) } } }
-        return api.map(tuplesAsDict)
+    static fileprivate func loadHackerspaceDataFromCache(url: String) -> Future<[String : JSONValue], SpaceAPIError> {
+        return loadFromCache(key: url).mapError { SpaceAPIError.unknownError(error: $0) }
+            .flatMap({ (data: Data) -> Result<[String : JSONValue], SpaceAPIError> in
+                JSONObject.parse(fromData: data)?.asObject |=> .dataCastError(data: data)
+            })
     }
 
     ///Returns a future containing a dictionary of names and endpoints of all hackerspaces using the SpaceAPI
-    static private func loadAPIFromCache() -> Future<[String : String], SpaceAPIError> {
+    static fileprivate func loadAPIFromCache() -> Future<[String : String], SpaceAPIError> {
         return loadFromCache(key: SpaceAPIConstants.FIXMEAPI.rawValue)
             .mapError({SpaceAPIError.unknownError(error: $0)})
             .flatMap { (data: Data) -> Result<[String : String], SpaceAPIError> in
                 parseAPI(data: data) |=> .dataCastError(data: data)
         }
-    }
-
-    static func loadAPIFromWeb() -> Future<[String : String], SpaceAPIError> {
-        return httpRequest(url: SpaceAPIConstants.FIXMEAPI.rawValue)
-            .mapError { SpaceAPIError.httpRequestError(error: $0) }
-            .flatMap { (data: Data) -> Result<[String: String], SpaceAPIError> in
-                return parseAPI(data: data) |=> SpaceAPIError.dataCastError(data: data)
-        }
-    }
-
-    static func loadHackerspaceList(fromCache: Bool) -> Future<[String: String], SpaceAPIError> {
-        return fromCache ? loadAPIFromCache() : (loadAPIFromWeb().recoverWith { _ in print("loading from web failed, loading from cache"); return loadAPIFromCache() })
-    }
-
-    static private func loadHackerspaceDataFromCache(url: String) -> Future<[String : JSONValue], SpaceAPIError> {
-        return loadFromCache(key: url).mapError { SpaceAPIError.unknownError(error: $0) }
-            .flatMap({ (data: Data) -> Result<[String : JSONValue], SpaceAPIError> in
-            JSONObject.parse(fromData: data)?.asObject |=> .dataCastError(data: data)
-        })
-    }
-
-    static private func loadHackerspaceDataFromWeb(url: String) -> Future<[String : JSONValue], SpaceAPIError> {
-
-        return httpRequest(url: url)
-            .mapError { SpaceAPIError.httpRequestError(error: $0) }
-            .flatMap({ (data: Data) -> Result<[String : JSONValue], SpaceAPIError> in
-                return JSONObject.parse(fromData: data)?.asObject |=> SpaceAPIError.dataCastError(data: data)
-            })
-    }
-
-    static func loadHackerspaceData(url: String, fromCache: Bool = true) -> Future<[String : JSONValue], SpaceAPIError> {
-        return (fromCache ? SpaceAPI.loadHackerspaceDataFromCache : SpaceAPI.loadHackerspaceDataFromWeb)(url)
-    }
-
-    static func parseHackerspace(json: [String : JSONValue], url: String, name: String) -> Result<ParsedHackerspaceData, SpaceAPIError>{
-        return parseHackerspaceDataModel(json: json, name: name, url: url).map(Result.init(value:)) ?? Result(error: SpaceAPIError.parseError(json.description))
-    }
-
-    static func getParsedHackerspace(url: String, name: String, fromCache cache: Bool = true) -> Future<ParsedHackerspaceData, SpaceAPIError> {
-        return loadHackerspaceData(url: url, fromCache: cache).flatMap { parseHackerspace(json: $0, url: url, name: name) }
     }
 }
