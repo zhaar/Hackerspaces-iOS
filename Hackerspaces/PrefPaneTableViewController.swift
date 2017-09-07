5edfd68c5d2838c14f07f933f939a82cc1c1c115
André Videla <andre.videla@gmail.com>
2017-09-07 12:24:12 +0200

    implement delete cache button in advanced options

--- Hackerspaces/PrefPaneTableViewController.swift
+++ Hackerspaces/PrefPaneTableViewController.swift
@@ -15,18 +15,40 @@ class PrefPaneTableViewController: UITableViewController {
             toggle.isOn = SharedData.isInDebugMode()
         }
     }
+
     @IBAction func toggleDebug(_ sender: UISwitch) {
 
         SharedData.toggleDebugMode()
         toggle.isOn = SharedData.isInDebugMode()
         if toggle.isOn {
-            let alert = UIAlertController(title: "Debug mode enabled", message: "debug mode allows you to access error details when a hackerspace is unreachable", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
+            let alert = UIAlertController(title: "Advanced mode enabled", message: "Advanced mode displays more advanced features useful for hackerspace API developers", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: constArg(tableView.reloadData)))
+            present(alert, animated: true, completion:         nil)
+        } else {
+            tableView.reloadData()
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return toggle.isOn ? 3 : 2
+        } else {
+            return 5
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected indexpath \(indexPath)")
+        if indexPath.section == 0, indexPath.row == 2 {
+            askConfirmation(for: SharedData.deleteAllDebug)
+        }
+    }
+
+
+    func askConfirmation(`for` action: @escaping () -> ()) -> () {
+        let alert = UIAlertController.init(title: "Deleting cache", message: "Are you sure you want to delete the local cache?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction.init(title: "Delete", style: .destructive, handler: constArg(action)))
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
